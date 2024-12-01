2233be0f8c99023d663e4dd86afece593f7d424a
rojhatbalta <rojhatbalta@gmail.com>
2024-12-01 14:40:54 +0300

    ContactList css changed

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,10 @@
 import Contact from "../Contact/Contact";
 import PropTypes from "prop-types";
-import "./ContactList.module.css";
+import Style from "./ContactList.module.css";
 
 export default function ContactList({ contacts, deleteContact }) {
   return (
-    <ul>
+    <ul className={Style.contactList}>
       {contacts.map((contact) => (
         <Contact
           contact={contact}
