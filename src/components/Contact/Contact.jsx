9ef8f4ebe64ed8160260e92b67011ff76931e950
rojhatbalta <rojhatbalta@gmail.com>
2024-12-08 20:19:51 +0300

    svg visibility change

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,33 +1,34 @@
 import PropTypes from "prop-types";
 import Style from "./Contact.module.css";
+import IconURL from "../../assets/icons.svg";
 
 export default function Contact({ contact, deleteContact }) {
   return (
     <li>
       <div className={Style.contactContainer}>
         <div>
-          <h2 className={Style.contactName}>
+          <div className={Style.contactItem}>
+            <svg
+              className={Style.contactSvg}
+              width="16"
+              height="16"
+              fill="#fffff"
+            >
+              <use href={IconURL + "#person"}></use>
+            </svg>
+            <h2 className={Style.contactName}>{contact.name}</h2>
+          </div>
+          <div className={Style.contactItem}>
             <svg
               className={Style.contactSvg}
               height="16"
               width="16"
               fill="#ffffff"
             >
-              <use href="../../assets/icons.svg#person"></use>
+              <use href={IconURL + "#phone"}></use>
             </svg>
-            {contact.name}
-          </h2>
-          <p className={Style.contactNumber}>
-            <svg
-              className={Style.contactSvg}
-              height="16"
-              width="16"
-              fill="#ffffff"
-            >
-              <use href="../../assets/icons.svg#phone"></use>
-            </svg>
-            {contact.number}
-          </p>
+            <p className={Style.contactNumber}>{contact.number}</p>
+          </div>
         </div>
         <button onClick={() => deleteContact(contact.id)}>Delete</button>
       </div>
